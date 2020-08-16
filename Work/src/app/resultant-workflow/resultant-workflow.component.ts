43fc4bdd06f8b3732d0a75be333ad21e5706d0be
manoj_snovasys <manoj@kothapalli.co.uk>
2020-08-16 14:23:59 +0530

    Workflow implemented

--- Work/src/app/resultant-workflow/resultant-workflow.component.ts
+++ Work/src/app/resultant-workflow/resultant-workflow.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WorkflowService } from '../services/workflow.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-resultant-workflow',
@@ -9,7 +10,37 @@ import { WorkflowService } from '../services/workflow.service';
 export class ResultantWorkflowComponent {
   components = [];
 
-  constructor(private workFlowService: WorkflowService) {
+  constructor(private workFlowService: WorkflowService, private router: Router) {
     this.components = this.workFlowService.components;
+  }
+
+  downloadCode() {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'app/resultant-workflow/resultant-workflow.component.html');
+    element.setAttribute('download', 'workflow.txt');
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+
+    var element = document.createElement('a');
+    element.setAttribute('href', 'app/resultant-workflow/resultant-workflow.component.ts');
+    element.setAttribute('download', 'workflow_ts.txt');
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+
+    var element = document.createElement('a');
+    element.setAttribute('href', '/styles.scss');
+    element.setAttribute('download', 'styles.txt');
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  }
+
+  goToMainPage() {
+    this.router.navigateByUrl('');
   }
 }
